19c3cd1db8e44c82a19bf0d8322f8d51b8a63786
Rasmus Juhl <rasmusjuhl@gmail.com>
2015-05-13 13:17:46 +0200

    Added a constructor for an undirected weighted graph and some code to add vertices and edges

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,22 @@ import org.jgrapht.graph.*;
 public class Main {
 
 	public static void main(String[] args) {
+		ListenableUndirectedWeightedGraph<String, DefaultWeightedEdge> myGraph = new ListenableUndirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 		
+		String s1 = "Rasmus";
+		String s2 = "Søren";
+		String s3 = "Silas";
+		String s4 = "Kasper";
+		
+		myGraph.addVertex(s1);
+		myGraph.addVertex(s2);
+		myGraph.addVertex(s3);
+		myGraph.addVertex(s4);
+		
+		myGraph.addEdge(s1, s2);
+		myGraph.addEdge(s1, s3);
+		myGraph.addEdge(s2, s4);
+		myGraph.addEdge(s3, s4);
 	}
 	
 
