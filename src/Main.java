0497f065bed125a42ec46094382151544c977134
Rasmus Juhl <rasmusjuhl@gmail.com>
2015-05-18 09:36:30 +0200

    Finished testing implementation of edges, vertices and dijkstras

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
+import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.*;
+
+import java.util.List;
 
 
 public class Main {
@@ -16,10 +21,29 @@ public class Main {
 		myGraph.addVertex(s3);
 		myGraph.addVertex(s4);
 		
-		myGraph.addEdge(s1, s2);
-		myGraph.addEdge(s1, s3);
-		myGraph.addEdge(s2, s4);
-		myGraph.addEdge(s3, s4);
+		DefaultWeightedEdge e1 = myGraph.addEdge(s1, s2);
+		DefaultWeightedEdge e2 = myGraph.addEdge(s1, s3);
+		DefaultWeightedEdge e3 = myGraph.addEdge(s2, s4);
+		DefaultWeightedEdge e4 = myGraph.addEdge(s3, s4);
+		
+		myGraph.setEdgeWeight(e1, 100);
+		myGraph.setEdgeWeight(e2, 75);
+		myGraph.setEdgeWeight(e3, 50);
+		myGraph.setEdgeWeight(e4, 25);
+		
+		DijkstraShortestPath<String, DefaultWeightedEdge> dijk = new DijkstraShortestPath<>(myGraph, s1, s4);
+		
+		Double shortestPath = dijk.getPathLength();
+		List<DefaultWeightedEdge> edgeList = dijk.getPathEdgeList();
+		
+		System.out.println("The shortest path found has length: " + shortestPath);
+		
+		System.out.println("The following route was found to be the shortest:");
+		
+		for(DefaultWeightedEdge edge : edgeList){
+			System.out.println(edge);
+		}
+		
 	}
 	
 
