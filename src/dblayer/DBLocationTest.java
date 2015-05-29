61d67b8890d098ad7019f032ea3e73739b372e23
Serec <soren.kaae@gmail.com>
2015-05-29 13:16:52 +0200

    DBLocationTEST fixed

--- src/dblayer/DBLocationTest.java
+++ src/dblayer/DBLocationTest.java
@@ -12,7 +12,7 @@ public class DBLocationTest {
 	public void testFindLocation()
 	{
 		DBLocation dbLoc = new DBLocation();
-		Location loc = dbLoc.findLocation("");
+		Location loc = dbLoc.findLocation("9000");
 		if(loc !=null)
 		{
 			System.out.println("Location found");
